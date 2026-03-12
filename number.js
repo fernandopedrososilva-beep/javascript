ceb06f65ae6d17b0563c4b334a55f3a21bd7c3f2
fernandopedrososilva-beep <fernando.pedroso.silva@escola.pr.gov.br>
2026-03-12 08:33:41 -0300

    Add files via upload

---
+++ number.js
@@ -0,0 +1,12 @@
+const meuNumero = 3
+
+const primeiroNumero = 1
+const segundoNumero = 2
+
+const operacaoMatematica = primeiroNumero + segundoNumero
+console.log(operacaoMatematica)
+
+const Numeropontoflutuante = 3.3
+const pontoflutuante = 5
+const novaoperacao = primeiroNumero / pontiflutuante
+console.log(novaoperacao)
